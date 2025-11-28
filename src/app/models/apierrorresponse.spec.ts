42def0a36a6778216e1e45d3241f89b73477a01f
Fabricio Quintana <fabriciorcq@gmail.com>
2025-11-28 10:50:15 -0300

    update

--- src/app/models/apierrorresponse.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Apierrorresponse } from './apierrorresponse';
-
-describe('Apierrorresponse', () => {
-  it('should create an instance', () => {
-    expect(new Apierrorresponse()).toBeTruthy();
-  });
-});
